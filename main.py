40e67eb965826952b25bad27f5a18edd5d560d0b
Mihkel Palta <Mihkel.Palta@edu.mayk.fi>
2025-03-20 08:44:01 +0200

    Palvelu muuttui laskimeksi

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+import random
 
 app = Flask(__name__)
 
@@ -8,5 +9,15 @@ def root():
 
 @app.route("/vastaus")
 def vastaus():
-    return render_template('vastaus.html', nimi=request.args['nimi'])
+    #uusi_nimi = request.args['nimi']
+    uusi_nimi = random.randint(1,100)
+    if uusi_nimi >= 70:
+        uusi_nimi = random.randint(1,10000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000)
+    #with open("kaikki_nimet.txt" , "a") as nimitiedosto:
+        #nimitiedosto.write(uusi_nimi + "   VALI   "+ "\n")
+        #kaikki_nimet = open('kaikki_nimet.txt').read()
+    return render_template('vastaus.html', nimi=uusi_nimi)
+    #return render_template('vastaus.html', nimi=request.args['nimi'])
 
+if __name__=="__main__":
+    app.run()
